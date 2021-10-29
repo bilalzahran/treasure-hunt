d78a36bb3cf08b554a841e57ff6b05170eb88688
bilalzahran <bilalzahran08@gmail.com>
2021-10-29 18:57:33 +0700

    add show probable location for the treasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import randint
 
-
+probable_location = []
 GRID_SIZE = 7
 MAX_TREASURE = 5
 grid = [
@@ -10,7 +10,7 @@ grid = [
     ["#", ".", ".", ".", ".", ".", ".", "#"],  # 2
     ["#", ".", ".", ".", ".", ".", ".", "#"],  # 3
     ["#", ".", "#", "#", "#", ".", ".", "#"],  # 4
-    ["#", ".", ".", ".", "#", ".", ".", "#"],  # 5
+    ["#", ".", ".", ".", "#", ".", "#", "#"],  # 5
     ["#", ".", "#", ".", ".", ".", ".", "#"],  # 6
     ["#", "#", "#", "#", "#", "#", "#", "#"],  # 7
 ]
@@ -48,12 +48,49 @@ def init_game():
     global treasure_list
     global player_location
     global treasure_count
-
     player_location = Point(6, 1)
     treasure_list = generate_treasure()
     treasure_count = 0
+    generate_probable_treasure_coordinate()
 
     return player_location, treasure_count, treasure_list
+
+
+def game_loop():
+    player_location, treasure_count, treasure_list = init_game()
+    print("Availabel Command:\n 1.up\n 2.down\n 3.right\n 4.left\n 5.check \n")
+    print("Your Treasure: ", treasure_count)
+    print(f"You are at {player_location.to_string()} now\n")
+    while True:
+        command = input(" Where to go: ")
+        command_combination = command.split(" ")
+
+        direction = command_combination[0].lower()
+        player_location = process_command(direction, player_location)
+
+        # Check if user not hitting the wall or out of bound
+        if not check_on_grid(player_location):
+            print("You are hitting the wall! Go back!")
+            player_location = process_command(
+                undo_direction_dict[direction], player_location)
+
+        # Update the probable location of treasure
+        update_probable_treasure_coordinate(player_location)
+
+        # Check if user get the treasure
+        treasure, treasure_status = check_treasure(
+            player_location, treasure_list)
+        if treasure_status:
+            print(f"Congrats! You got {treasure} treasure!")
+            treasure_count += treasure
+
+        # Check if user get all the treasure, if yes end the game
+        if treasure_count == MAX_TREASURE:
+            print("You got all treasure!")
+            break
+
+        print("\nYour treasure: ", treasure_count)
+        print(f"You are at {player_location.to_string()} now \n")
 
 
 def check_on_grid(point: Point):
@@ -96,6 +133,8 @@ def process_command(direction: str, player_location: Point):
         player_location.add_y(1)
     elif direction == "show":
         show_grid_with_treasure()
+    elif direction == "check":
+        check_probable_treasure_coordinate()
     else:
         print("I dont understand the direction")
 
@@ -103,39 +142,34 @@ def process_command(direction: str, player_location: Point):
 
 
 def check_treasure(player_location: Point, treasure_list):
+    treasure = 0
     for item in treasure_list:
         item: Point = item
         if(item.is_equal(player_location)):
-            return True
-
-    return False
-
-
-def game_loop():
-    player_location, treasure_count, treasure_list = init_game()
-    print("Insert Command: ", treasure_count)
-    print(f"You are at {player_location.to_string()} now\n")
-    while True:
-        command = input(" Where to go: ")
-        command_combination = command.split(" ")
-
-        direction = command_combination[0].lower()
-        player_location = process_command(direction, player_location)
-
-        if not check_on_grid(player_location):
-            print("You are hitting the wall! Go back!")
-            player_location = process_command(
-                undo_direction_dict[direction], player_location)
-
-        if check_treasure(player_location, treasure_list):
-            print("You got one treasure!")
-            treasure_count += 1
-
-        if treasure_count == MAX_TREASURE:
-            print("You got all treasure!")
-            break
-        print("\nYour treasure: ", treasure_count)
-        print(f"You are at {player_location.to_string()} now \n")
+            treasure += 1
+    if treasure > 0:
+        return treasure, True
+    else:
+        return 0, False
 
 
-game_loop()
+def generate_probable_treasure_coordinate():
+    for i, row in enumerate(grid):
+        for j, col in enumerate(row):
+            if col == ".":
+                probable_location.append(Point(i, j))
+
+
+def check_probable_treasure_coordinate():
+    for item in probable_location:
+        print(item.to_string())
+
+
+def update_probable_treasure_coordinate(point: Point):
+    for i, item in enumerate(probable_location):
+        if item.is_equal(point):
+            probable_location.pop(i)
+
+
+if __name__ == "__main__":
+    game_loop()
